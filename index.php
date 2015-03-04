fda2718285c2c155f122c744162e8f9baf21c0fb
CristinaJuhee <juhee940@gmail.com>
2015-03-04 10:06:08 -0300

    Exercicio - 1C

--- index.php
+++ index.php
@@ -1,11 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+		     table,td{border:solid 1px black;}
+			 
+	     </style>
+    </head>
+    
+    <body>
+        <h1> Exercício1 - C </h1>
+        <table>
+            <tr>
+                <td font-style="bord">ID</td>
+                <td font-style="bord">NOME</td>
+<td font-style="bord">DESC</td>
+            </tr>
+			
+			<tr>
+                <td>id</td>
+                <td>nome</td>
+<td>desc</td>
+			
+            <tr>
+                <td bgcolor=”#C0C0C0”>id</td>
+                <td bgcolor=”#C0C0C0”>nome</td>
+<td bgcolor=”#C0C0C0”>desc</td>
+            </tr>
+            
+            <tr>
+                <td>id</td>
+                <td>nome</td>
+<td>desc</td>
+
+                
+            </tr>
+            <tr>
+                <td bgcolor=”#C0C0C0”>id</td>
+                <td bgcolor=”#C0C0C0”>nome</td>
+                <td bgcolor=”#C0C0C0”>desc</td>
+
+            </tr>
+            <tr>
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+
+            </tr>
+            <tr>
+                <td bgcolor=”#C0C0C0”>id</td>
+                <td bgcolor=”#C0C0C0”>nome</td>
+                <td bgcolor=”#C0C0C0”>desc</td>
+
+            </tr>
+			
+			<tr>
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+
+            </tr>
+			
+        </table>
+         
+       
+    </body>
+</html>
+
+
+
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-  echo 12314;
-  echo '<font color="red"> CristinaJuhee </font>';
   
 ?>
